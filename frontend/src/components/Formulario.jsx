c2b6dd0e44e6242fbe8eec6aa392a7107b9311e9
fernandomdj <luis.md@ucad.edu.mx>
2026-08-11 12:31:01 -0600

    se agregaron los codigos de cada archivo de de la carpeta components

---
+++ frontend/src/components/Formulario.jsx
@@ -0,0 +1,95 @@
+function Formulario ({datos, setDatos, onSumit, textoBoton}) {
+    function manejarCambio(evento){
+        const {name, value} = evento.target;
+
+        setDatos({
+            ...datos,
+            [name]:value
+        });
+    } 
+
+return (
+    <form className="formulario" onSubmit={onSubmit}>
+        
+        <label>
+            Titulo
+            <input 
+            type="text"
+            name="titulo"
+            value={datos.titulo}
+            onChange={manejarCambio}
+            required
+            />
+        </label>
+
+        <label>
+            Año
+            <input
+            type="number"
+            name= "anio"
+            value={datos.anio}
+            onChange={manejarCambio}
+            required
+            />
+        </label>
+
+        <label>
+            Genero 
+            <input 
+            type="text"
+            name="genero"
+            value={datos.genero}
+            onChange={manejarCambio}
+            required
+            />
+        </label>
+
+        <label>
+            Director
+            <input 
+            type="text"
+            name="director"
+            value={datos.director}
+            onChange={manejarCambio}
+            required
+            />
+        </label>
+         
+        <label>
+            duración en minutos
+            <input
+            type="number"
+            name="duracion"
+            value={datos.duracion}
+            onChange={manejarCambio}
+            required
+            />  
+        </label>
+        
+        <label>
+            Clasificación
+            <input
+            type="text"
+            name="clasificacion"
+            onChange={manejarCambio}
+            required
+            />
+        </label>
+
+        <label>
+            URL del póster
+            <input
+            type="url"
+            name="imagen"
+            value={datos.imagen}
+            onChange={manejarCambio}
+            required
+            />
+        </label>
+
+        <button type="submit">{textoBoton} </button>
+    </form>
+    );
+}
+
+export default Formulario; 
